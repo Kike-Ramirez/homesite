fccdae749fcd8458b83b6291d98f0cb4d65a695f
Kike.Ramirez <kike@vjspain.com>
2018-03-09 02:54:10 +0100

    3d model added

--- public/javascripts/TimeWheel.js
+++ public/javascripts/TimeWheel.js
@@ -1,17 +1,24 @@
+
 if (!Detector.webgl) Detector.addGetWebGLMessage();
+
 var container, stats, controls;
 var camera, scene, renderer, light;
 var clock = new THREE.Clock();
 var mixers = [];
+
 init();
 animate();
+
 function init() {
+
     container = document.createElement('div');
     document.body.appendChild(container);
-    camera = new THREE.PerspectiveCamera(45, window.innerWidth / window.innerHeight, 1, 2000);
-    camera.position.set(100, 200, 300);
+    camera = new THREE.PerspectiveCamera(45, window.innerWidth / window.innerHeight, 1, 10000);
+    camera.position.z = 2;
+    camera.position.y = 4;
+
     controls = new THREE.OrbitControls(camera);
-    controls.target.set(0, 100, 0);
+    controls.target.set(0, 0, 0);
     controls.update();
     scene = new THREE.Scene();
     scene.background = new THREE.Color(0xa0a0a0);
@@ -28,53 +35,70 @@ function init() {
     light.shadow.camera.right = 120;
     scene.add(light);
     // scene.add( new THREE.CameraHelper( light.shadow.camera ) );
+
     // ground
     var mesh = new THREE.Mesh(new THREE.PlaneGeometry(2000, 2000), new THREE.MeshPhongMaterial({ color: 0x999999, depthWrite: false }));
     mesh.rotation.x = - Math.PI / 2;
     mesh.receiveShadow = true;
     scene.add(mesh);
+
     var grid = new THREE.GridHelper(2000, 20, 0x000000, 0x000000);
     grid.material.opacity = 0.2;
     grid.material.transparent = true;
     scene.add(grid);
+
     // model
-    var loader = new THREE.FBXLoader();
-    loader.load('3d/clockwork/Clockwork.fbx', function (object) {
-        object.mixer = new THREE.AnimationMixer(object);
-        mixers.push(object.mixer);
-        // var action = object.mixer.clipAction(object.animations[0]);
-        // action.play();
-        object.traverse(function (child) {
-            if (child.isMesh) {
-                child.castShadow = true;
-                child.receiveShadow = true;
+    //3ds files dont store normal maps
+    var loader = new THREE.TextureLoader();
+    var normal = loader.load( '../3d/portalgun/textures/normal.jpg' );
+    var loader = new THREE.TDSLoader( );
+    loader.setPath( '../3d/portalgun/textures/' );
+    loader.load( '../3d/portalgun/portalgun.3ds', function ( object ) {
+        object.traverse( function ( child ) {
+            if ( child instanceof THREE.Mesh ) {
+                child.material.normalMap = normal;
             }
-        });
-        scene.add(object);
+        } );
+        object.scale = (100, 100, 100);
+        object.position = (0, 2, 0)
+        scene.add( object );
     });
+
+
     renderer = new THREE.WebGLRenderer();
     renderer.setPixelRatio(window.devicePixelRatio);
     renderer.setSize(window.innerWidth, window.innerHeight);
     renderer.shadowMap.enabled = true;
     container.appendChild(renderer.domElement);
     window.addEventListener('resize', onWindowResize, false);
+
     // stats
     stats = new Stats();
     container.appendChild(stats.dom);
 }
+
 function onWindowResize() {
+
     camera.aspect = window.innerWidth / window.innerHeight;
     camera.updateProjectionMatrix();
     renderer.setSize(window.innerWidth, window.innerHeight);
+
 }
+
 //
 function animate() {
+
     requestAnimationFrame(animate);
+
     if (mixers.length > 0) {
+
         for (var i = 0; i < mixers.length; i++) {
             mixers[i].update(clock.getDelta());
         }
+
     }
+
     renderer.render(scene, camera);
     stats.update();
+
 }
