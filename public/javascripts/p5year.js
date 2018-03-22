997636240768a56c571a1009f85a7ea75b961ad9
kikeRamirez <kike.ramirez@fullsix.com>
2018-03-22 18:03:50 +0100

    p5.js added to wireframe

---
+++ public/javascripts/p5year.js
@@ -0,0 +1,39 @@
+var bugs = []; // array of Jitter objects
+
+function setup() {
+
+  cnv = createCanvas(width, height);
+
+  // Move the canvas so it’s inside our <div id="year_panel">.
+  cnv.parent('year_panel');
+
+  // Create objects
+  for (var i=0; i<50; i++) {
+    bugs.push(new Year());
+  }
+}
+
+function draw() {
+  background(50, 89, 100);
+  for (var i=0; i<bugs.length; i++) {
+    bugs[i].move();
+    bugs[i].display();
+  }
+}
+
+// Year class
+function Year() {
+  this.x = random(width);
+  this.y = random(height);
+  this.diameter = random(10, 30);
+  this.speed = 1;
+
+  this.move = function() {
+    this.x += random(-this.speed, this.speed);
+    this.y += random(-this.speed, this.speed);
+  };
+
+  this.display = function() {
+    ellipse(this.x, this.y, this.diameter, this.diameter);
+  };
+}
